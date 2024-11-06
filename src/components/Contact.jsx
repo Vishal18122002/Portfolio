3f271d19d729555470d4eb53b31df5b0edc4ff2f
Vishal.G <100464577+Vishal18122002@users.noreply.github.com>
2024-11-06 09:33:55 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -85,7 +85,7 @@ const Contact = () => {
             <a href="https://www.linkedin.com/in/vishalg1812/" target="_blank" rel="noopener noreferrer" className="no-underline hover:underline mr-4">LinkedIn</a>
             <a href="https://leetcode.com/u/vishal1812--/" target="_blank" rel="noopener noreferrer" className="no-underline hover:underline mr-4">LeetCode</a>
             <a href="https://github.com/Vishal18122002" target="_blank" rel="noopener noreferrer" className="no-underline hover:underline mr-4">GitHub</a>
-            <a href="https://drive.google.com/file/d/1Bd3qUHRHPo8t8I00vJVXMYIYVdr2Lg2M/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="no-underline hover:underline">Resume</a>
+            <a href="https://drive.google.com/file/d/1h7gSnOclBoTTJlRPogy_LRrV75KxOX4t/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="no-underline hover:underline">Resume</a>
           </div>
         </div>
       </motion.div>
